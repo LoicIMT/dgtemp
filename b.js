4134d75c0e119a73b108d1bb43a862338eb59767
LoicIMT <92547427+LoicIMT@users.noreply.github.com>
2023-11-19 01:39:15 +0100

    Create b.js

---
+++ b.js
@@ -0,0 +1,11 @@
+const xhr = new XMLHttpRequest();
+xhr.open("GET", window.location.origin+"/activate-plugin/1");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    console.log(xhr.response);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
